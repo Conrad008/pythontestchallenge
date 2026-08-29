3e49a86f3a009ef216a91f025b5711921e8de3f4
Conrad008 <kipngeno7180@gmail.com>
2026-08-29 19:43:27 +0300

    feat:Add UserFactory for testing

--- pytestchallengeApp/tests/factories.py
+++ pytestchallengeApp/tests/factories.py
@@ -1,2 +1,21 @@
 import factory
 from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(
+        lambda n: f"user{n}"
+    )
+
+    email = factory.LazyAttribute(
+        lambda user: f"{user.username}@example.com"
+    )
+
+    password = factory.django.Password(
+        "Password123!"
+    )
