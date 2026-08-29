c6aa8ee0ff28cc0341c6d982903249687d514e63
Conrad008 <kipngeno7180@gmail.com>
2026-08-29 21:12:22 +0300

    test: add unit test for existing user password reset request

--- pytestchallengeApp/tests/test_reset_request.py
+++ pytestchallengeApp/tests/test_reset_request.py
@@ -2,3 +2,18 @@ import pytest
 from django.urls import reverse
 
 from .factories import UserFactory
+
+@pytest.mark.django_db
+def test_existing_user_can_request_password_reset(client):
+    user = UserFactory(
+        email="Conrad@example.com"
+    )
+
+    response = client.post(
+        reverse("password-reset-request"),
+        {
+            "email": user.email
+        }
+    )
+
+    assert response.status_code == 200
