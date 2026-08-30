289412a86662587c77d215c44cd6d08530cf1647
Conrad008 <kipngeno7180@gmail.com>
2026-08-30 17:27:10 +0300

    feat: implement password reset request logic

--- pytestchallengeApp/views.py
+++ pytestchallengeApp/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 
+User = get_user_model()
 
 def password_reset_request(request):
+    email = request.POST.get("email")
+
+    user = User.objects.filter(
+        email=email
+    ).first()
+
+    if user:
+        return HttpResponse(
+            "Password reset email sent"
+        )
+
     return HttpResponse("Password reset request received")
