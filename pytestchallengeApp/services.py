43718eba897c04948859fda36779958295d27c60
Conrad008 <kipngeno7180@gmail.com>
2026-08-31 13:39:03 +0300

    refactor: improve code formatting for better readability in services.py

--- pytestchallengeApp/services.py
+++ pytestchallengeApp/services.py
@@ -1,8 +1,12 @@
-from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.tokens import (
+    default_token_generator,
+)
 from django.core.mail import send_mail
 from django.urls import reverse
 from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import (
+    urlsafe_base64_encode,
+)
 
 
 def build_reset_url(request, user):
@@ -10,8 +14,10 @@ def build_reset_url(request, user):
         force_bytes(user.pk)
     )
 
-    token = default_token_generator.make_token(
-        user
+    token = (
+        default_token_generator.make_token(
+            user
+        )
     )
 
     reset_path = reverse(
@@ -27,11 +33,14 @@ def build_reset_url(request, user):
     )
 
 
-def send_reset_email(email, reset_url):
+def send_reset_email(
+    email,
+    reset_url
+):
     subject = "Reset your password"
 
     message = (
-        f"Use the following link to reset "
+        "Use the following link to reset "
         f"your password:\n\n{reset_url}"
     )
 
