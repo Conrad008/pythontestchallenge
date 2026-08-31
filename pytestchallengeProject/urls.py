7c288d8b6a02d3d501f61e6a135f4693310a2497
Conrad008 <kipngeno7180@gmail.com>
2026-08-31 21:52:36 +0300

    feat: add home view to redirect to password reset request

--- pytestchallengeProject/urls.py
+++ pytestchallengeProject/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect("password-reset-request")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home, name="home"),
     path("", include("pytestchallengeApp.urls")),
 ]
