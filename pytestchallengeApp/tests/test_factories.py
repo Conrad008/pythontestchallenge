73cc7c4df04ec7f3ac4d41bce1ad5c99cf1990eb
Conrad008 <kipngeno7180@gmail.com>
2026-08-31 13:42:53 +0300

    refactor: rename test function for clarity and update password check

--- pytestchallengeApp/tests/test_factories.py
+++ pytestchallengeApp/tests/test_factories.py
@@ -4,9 +4,12 @@ from .factories import UserFactory
 
 
 @pytest.mark.django_db
-def test_user_factory_creates_user():
+def test_user_factory_creates_user_with_hashed_password():
     user = UserFactory()
 
     assert user.username is not None
     assert user.email is not None
-    assert user.check_password("Password123!") is True
+
+    assert user.check_password(
+        "StrongPass123!"
+    ) is True
