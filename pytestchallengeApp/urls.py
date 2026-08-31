136e4f5829d8dd90231e1eb7e376fc13bf1265ca
Conrad008 <kipngeno7180@gmail.com>
2026-08-31 13:38:27 +0300

    feat: reorder password reset URL patterns for improved clarity

--- pytestchallengeApp/urls.py
+++ pytestchallengeApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import (
-    password_reset_request,
+    password_reset_complete,
     password_reset_confirm,
+    password_reset_request,
 )
+
 
 urlpatterns = [
     path(
@@ -13,8 +15,16 @@ urlpatterns = [
     ),
 
     path(
-        "password-reset/<str:uidb64>/<str:token>/",
+        "password-reset/"
+        "<str:uidb64>/"
+        "<str:token>/",
         password_reset_confirm,
         name="password-reset-confirm",
+    ),
+
+    path(
+        "password-reset-complete/",
+        password_reset_complete,
+        name="password-reset-complete",
     ),
 ]
