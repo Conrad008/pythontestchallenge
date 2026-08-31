33f5949328157b6bbc81d73ff39d20473b11ec70
Conrad008 <kipngeno7180@gmail.com>
2026-08-31 11:48:46 +0300

    feat: organize imports and add password reset confirmation URL

--- pytestchallengeApp/urls.py
+++ pytestchallengeApp/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 
-from .views import password_reset_request
-
+from .views import (
+    password_reset_request,
+    password_reset_confirm,
+)
 
 urlpatterns = [
     path(
         "password-reset/",
         password_reset_request,
         name="password-reset-request",
+    ),
+
+    path(
+        "password-reset/<str:uidb64>/<str:token>/",
+        password_reset_confirm,
+        name="password-reset-confirm",
     ),
 ]
