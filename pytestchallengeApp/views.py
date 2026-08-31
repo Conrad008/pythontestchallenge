44b437fed507556f6dc1bdae240aee93a0f2412d
Conrad008 <kipngeno7180@gmail.com>
2026-08-31 13:37:52 +0300

    feat: refactor password reset views for improved structure and readability

--- pytestchallengeApp/views.py
+++ pytestchallengeApp/views.py
@@ -1,30 +1,28 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.tokens import default_token_generator
-from django.shortcuts import render
+from django.contrib.auth.tokens import (
+    default_token_generator,
+)
+from django.shortcuts import (
+    redirect,
+    render,
+)
 from django.utils.encoding import force_str
-from django.utils.http import urlsafe_base64_decode
+from django.utils.http import (
+    urlsafe_base64_decode,
+)
 
-from .forms import PasswordResetRequestForm
+from .forms import (
+    NewPasswordForm,
+    PasswordResetRequestForm,
+)
 from .services import (
     build_reset_url,
     send_reset_email,
 )
 
+
 User = get_user_model()
 
-# def password_reset_request(request):
-#     email = request.POST.get("email")
-
-#     user = User.objects.filter(
-#         email=email
-#     ).first()
-
-#     if user:
-#         return HttpResponse(
-#             "Password reset email sent"
-#         )
-
-#     return HttpResponse("Password reset request received")
 
 def password_reset_request(request):
     message = None
@@ -55,8 +53,9 @@ def password_reset_request(request):
                 )
 
             message = (
-                "If an account exists for that email, "
-                "a password reset link has been sent."
+                "If an account exists for that "
+                "email, a password reset link "
+                "has been sent."
             )
 
     else:
@@ -71,11 +70,8 @@ def password_reset_request(request):
         },
     )
 
-def password_reset_confirm(
-    request,
-    uidb64,
-    token
-):
+
+def get_user_from_uid(uidb64):
     try:
         user_id = force_str(
             urlsafe_base64_decode(
@@ -83,7 +79,7 @@ def password_reset_confirm(
             )
         )
 
-        user = User.objects.get(
+        return User.objects.get(
             pk=user_id
         )
 
@@ -93,7 +89,17 @@ def password_reset_confirm(
         OverflowError,
         User.DoesNotExist,
     ):
-        user = None
+        return None
+
+
+def password_reset_confirm(
+    request,
+    uidb64,
+    token
+):
+    user = get_user_from_uid(
+        uidb64
+    )
 
     token_is_valid = (
         user is not None
@@ -108,15 +114,53 @@ def password_reset_confirm(
             request,
             "reset_password.html",
             {
-                "token_valid": False
+                "token_valid": False,
             },
             status=400,
-             )
+        )
+
+    if request.method == "POST":
+        form = NewPasswordForm(
+            request.POST,
+            user=user
+        )
+
+        if form.is_valid():
+            new_password = (
+                form.cleaned_data[
+                    "password1"
+                ]
+            )
+
+            user.set_password(
+                new_password
+            )
+
+            user.save(
+                update_fields=["password"]
+            )
+
+            return redirect(
+                "password-reset-complete"
+            )
+
+    else:
+        form = NewPasswordForm(
+            user=user
+        )
 
     return render(
         request,
         "reset_password.html",
         {
-            "token_valid": True
+            "token_valid": True,
+            "form": form,
         },
+    )
+
+
+def password_reset_complete(request):
+    return render(
+        request,
+        "password_reset_complete.html"
     )
